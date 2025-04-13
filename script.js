8a8676f83d381f23c7c3c08ed9ca999bdc7f7f00
Elisson15 <148507784+Elisson15@users.noreply.github.com>
2025-04-13 16:39:17 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function comprarAgora() {
+  alert("Você será redirecionado para a página de pagamento em breve.");
+  // Aqui você pode redirecionar para uma URL real de checkout
+  // window.location.href = "https://seupagamento.com/link-do-curso";
+}
